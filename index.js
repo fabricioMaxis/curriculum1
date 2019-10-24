27e7e84cac635b71de8888e25112efd46cb53e99
fabricioMaxis <falus09@gmail.com>
2019-10-24 11:47:41 -0300

    Add port as env variable.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const app =express()
+const port = process.env.Port || 8000
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -9,7 +10,7 @@ app.get('/',(request,response) => {
         response.render('home')
 })
 
-app.listen(8000, (err) =>{
+app.listen(port, (err) =>{
     if (err){
         console.log('Não foi possivel localizar o servidor')
     }else{
